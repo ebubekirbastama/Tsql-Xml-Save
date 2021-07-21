fd805b1f070fd497bd1e87dfa22f7d2910678f1f
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-07-21 16:58:33 +0300

    Create YetkilendirmeTsql.sql

---
+++ YetkilendirmeTsql.sql
@@ -0,0 +1,15 @@
+--www.ebubekirbastama.com
+--İletşim:05554128854
+--www.twitter.com/ebubekirstt
+--https://www.youtube.com/user/Yazilimegitim/
+
+------- EBS Yetkilendirme -----------
+sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+sp_configure 'Ole Automation Procedures', 1;
+GO
+RECONFIGURE;
+GO
+------- EBS Yetkilendirme -----------
